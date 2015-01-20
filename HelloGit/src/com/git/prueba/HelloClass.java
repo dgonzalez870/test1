93edf2fb472e1e1cb29a1209187b7d5201f31c47
Dennys Gonzalez <dgonzalez870@yahoo.com>
2015-01-19 21:58:10 -0430

    Primer commit

---
+++ HelloGit/src/com/git/prueba/HelloClass.java
@@ -0,0 +1,11 @@
+package com.git.prueba;
+
+public class HelloClass {
+	public static void main(String[] args){
+		System.out.println("Hola git");
+		System.out.println("Otra prueba de Git");
+		System.out.println("Hola");
+		System.out.println("Hola 2");
+	}
+
+}
